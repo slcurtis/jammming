5441d8029544b7485a1bea044d97601b1df97b53
slcurtis <scwmbg@yahoo.com>
2017-12-06 13:27:18 -0500

    Changes for New Playlist.

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -18,9 +18,9 @@ class Playlist extends React.Component {
   render() {
     return (
       <div className="Playlist">
-        <input id="Playlist-name" defaultValue={this.props.playlistName} onChange={this.handleNameChange} />
+        <input value= "New Playlist" onChange={this.handleNameChange} />
         <TrackList tracks={this.props.playlistTracks} onRemove={this.props.onRemove} />
-        <a class="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
+        <a className="Playlist-save" onClick={this.props.onSave}>SAVE TO SPOTIFY</a>
       </div>
     );
   }
